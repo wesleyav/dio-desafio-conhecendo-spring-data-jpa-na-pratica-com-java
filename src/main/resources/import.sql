2eda68c9cc94181708555e2f24e17281119fcb39
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-08-03 11:41:45 -0300

    Adiciona script para popular base de dados de teste H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,35 @@
+# Insert tabela Aluno
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 1', '123.456.789-11', '2006-02-11', 'Aluno 1');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 2', '123.456.789-12', '2006-02-12', 'Aluno 2');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 3', '123.456.789-13', '2006-02-13', 'Aluno 3');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 4', '123.456.789-14', '2006-02-14', 'Aluno 4');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 5', '123.456.789-15', '2006-02-15', 'Aluno 5');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 6', '123.456.789-16', '2006-02-16', 'Aluno 6');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 7', '123.456.789-17', '2006-02-17', 'Aluno 7');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 8', '123.456.789-18', '2006-02-18', 'Aluno 8');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 9', '123.456.789-19', '2006-02-19', 'Aluno 9');
+insert into tb_aluno (bairro, cpf, data_de_nascimento, nome) values ('bairro 10', '123.456.789-10', '2006-02-10', 'Aluno 10');
+
+# Insert tabela Avaliacao
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.71, '2006-02-01', 71, 1);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.72, '2006-02-02', 72, 2);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.73, '2006-02-03', 73, 3);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.74, '2006-02-04', 74, 4);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.75, '2006-02-05', 75, 5);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.76, '2006-02-06', 76, 6);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.77, '2006-02-07', 77, 7);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.78, '2006-02-08', 78, 8);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.79, '2006-02-09', 79, 8);
+insert into tb_avaliacao(altura_atual, data_da_avaliacao, peso_atual, aluno_id) values (1.80, '2006-02-10', 80, 10);
+
+# Insert tabela Matricula
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-01', 1);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-02', 2);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-03', 3);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-04', 4);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-05', 5);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-06', 6);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-07', 7);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-08', 8);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-09', 9);
+insert into tb_matricula(data_da_matricula, aluno_id) values ('2006-02-10', 10);
